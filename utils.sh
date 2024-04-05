c2e22ac86e26c1091d047c2d8b5a4f33d49f3add
gustnv <nunesvianagustavo@gmail.com>
2024-04-05 17:24:37 -0300

    fix: multiple bash compatible

--- utils.sh
+++ utils.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-# Add the alias to .bashrc
-echo "alias asm='java -jar ../mars.jar'" >> ~/.bashrc
-echo "alias mars='java -jar mars.jar'" >> ~/.bashrc
+# Function to add an alias to the appropriate config file
+add_alias() {
+  local alias_name="$1"
+  local alias_command="$2"
 
-# Restart the shell
-source ~/.bashrc
+  # Check if we're using Zsh
+  if [[ -n $ZSH_VERSION ]]; then 
+    echo "alias $alias_name='$alias_command'" >> ~/.zshrc
+  else
+    echo "alias $alias_name='$alias_command'" >> ~/.bashrc 
+  fi
+}
+
+# Add the 'asm' alias
+add_alias "asm" "java -jar ../mars.jar"
+
+# Add the 'mars' alias
+add_alias "mars" "java -jar mars.jar"
+
+# Restart the shell to apply changes
+source ~/.zshrc  # Source .zshrc for Zsh
+source ~/.bashrc  # Source .bashrc for Bash
