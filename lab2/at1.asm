a5405663547e456ac38c43ccf4bab1522edc262e
gustnv <nunesvianagustavo@gmail.com>
2024-04-11 21:42:56 -0300

    feat: at1

--- lab2/at1.asm
+++ lab2/at1.asm
@@ -1,22 +1,17 @@
 .data
-
+	# Lookup table
+	seven_seg_table: .byte 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F
 .text
 # Register Usage:
-# $s0 - command right seven segment display 	
-# $s1 - number value
+# $s0 - command right seven segment display
+# $t0 - counter
+# $t1 - number value
 
-	li $s1, 0x3F
-	j Loop
+	li $s0, 0xFFFF0010 # Load the display address
 	
-	li $s1, 0x6
-	li $s1, 0x5B
-	li $s1, 0x4F
-	li $s1, 0x66
-	li $s1, 0x6D
-	li $s1, 0x7D
-	li $s1, 0x07
-	li $s1, 0x7F
-	li $s1, 0x6F
-   Abcd:
-   Loop:li $s0, 0xFFFF0010
-	sb $s1, 0($s0)
+	# Loop to display all digits:
+  	display_loop:
+    	lbu $t1, seven_seg_table($t0)  # Load value from the table
+    	sb $t1, 0($s0)                 # Store it in the display address
+    	addi $t0, $t0, 1               # Increment the index for the next digit
+    	blt $t0, 10, display_loop      # Repeat if there are more digits to display
