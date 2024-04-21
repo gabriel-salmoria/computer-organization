ed85de8fee0c33f44b441fd0a6129f3d69c32f8b
gustnv <nunesvianagustavo@gmail.com>
2024-04-21 18:37:18 -0300

    feat: lab3 comments

--- lab3/at1.asm
+++ lab3/at1.asm
@@ -1,30 +1,30 @@
 .data
-	a: .word 4
-	b: .word 5
+    	a: .word 4   	    # Define uma variável a com o valor 4
+    	b: .word 5          # Define uma variável b com o valor 5
 
 .text
-	lw $a0, a
-	lw $a1, b
-	jal Multiplicacao
-	j Exit
-	
-Multiplicacao:
-	addi $sp, $sp -8
-	sw $a1, 4($sp)
-	sw $ra, 0($sp)
-	
-	bne $a1, $zero, If
-	
-	li $v0, 0
-	j EndIf
-    If:
-    	addi $a1, $a1, -1
-    	jal Multiplicacao
-    	add $v0, $a0, $v0
-     	
+    	lw $a0, a           # Carrega o valor de a em $a0
+    	lw $a1, b           # Carrega o valor de b em $a1
+    	jal Multiplicacao   # Chama a função Multiplicacao
+    	j Exit       	    # Salta para o ponto de saída do programa
+
+    Multiplicacao:
+    	addi $sp, $sp, -8   # Aloca espaço na pilha para salvar $a1 e $ra
+    	sw $a1, 4($sp)      # Salva o valor de $a1 na pilha
+    	sw $ra, 0($sp)      # Salva o endereço de retorno na pilha
+    
+    	bne $a1, $zero, If  # Se $a1 não for zero, salta para If
+    
+    	li $v0, 0           # Se $a1 for zero, define $v0 como 0 (caso base)
+   	j EndIf             # Salta para o final da função
+    If:	
+   	addi $a1, $a1, -1   # Decrementa $a1
+  	jal Multiplicacao   # Chama recursivamente a função Multiplicacao
+  	add $v0, $a0, $v0   # Soma $a0 com o resultado da chamada recursiva
+    
     EndIf:
-	lw $ra, 0($sp)
-	lw $a1, 4($sp)
-	addi $sp, $sp, 8
-	jr $ra
-Exit:
+    	lw $ra, 0($sp)      # Restaura o endereço de retorno
+    	lw $a1, 4($sp)      # Restaura o valor de $a1
+    	addi $sp, $sp, 8    # Desaloca o espaço da pilha
+    	jr $ra              # Retorna para o endereço de retorno
+    Exit:
--- lab3/at2.asm
+++ lab3/at2.asm
@@ -1,31 +1,34 @@
 .data
-	v: .word 11 2 3 14 15
-	n: .word 5
+    	v: .word 11 2 3 14 15  # Array de números
+    	n: .word 5             # Tamanho do array
 
 .text
-	lw $a0, n
-	jal Soma
-	j Exit
-Soma:
-	addi $sp, $sp -8
-	sw $a0, 4($sp)
-	sw $ra, 0($sp)
-	
-	li $t0, 1
-	bne $a0, $t0, If
-	
-	lw $v0, v($zero)
-	j EndIf
+    	lw $a0, n             # Carrega o tamanho do array em $a0
+    	jal Soma              # Chama a função Soma
+    	j Exit                # Salta para o ponto de saída do programa
+
+    Soma:
+    	addi $sp, $sp, -8     # Aloca espaço na pilha para salvar $a0 e $ra
+    	sw $a0, 4($sp)        # Salva o valor de $a0 na pilha
+    	sw $ra, 0($sp)        # Salva o endereço de retorno na pilha
+
+    	li $t0, 1             # Carrega o valor 1 em $t0
+    	bne $a0, $t0, If      # Se $a0 não for igual a 1, salta para If
+
+    	lw $v0, v($zero)      # Se $a0 for 1, carrega o primeiro elemento do array em $v0
+    	j EndIf               # Salta para o final da função
+
     If:
-    	addi $a0, $a0, -1
-    	jal Soma
-    	sll $a0, $a0, 2
-    	lw $t0, v($a0)
-    	add $v0, $t0, $v0
-     	
+    	addi $a0, $a0, -1     # Decrementa $a0
+    	jal Soma              # Chama recursivamente a função Soma
+    	sll $a0, $a0, 2       # Multiplica $a0 por 4 para calcular o deslocamento no array de palavras
+    	lw $t0, v($a0)        # Carrega o elemento do array usando o deslocamento calculado
+    	add $v0, $t0, $v0     # Soma o elemento atual do array com o resultado da chamada recursiva
+
     EndIf:
-	lw $ra, 0($sp)
-	lw $a0, 4($sp)
-	addi $sp, $sp, 8
-	jr $ra
-Exit:
+    	lw $ra, 0($sp)        # Restaura o endereço de retorno
+    	lw $a0, 4($sp)        # Restaura o valor de $a0
+    	addi $sp, $sp, 8      # Desaloca o espaço da pilha
+    	jr $ra                # Retorna para o endereço de retorno
+
+    Exit:
