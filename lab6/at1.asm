0fdc60779cb49e780f1be30c1d3227d02853c306
gabriel-salmoria <gabrielsalmoria1@gmail.com>
2024-05-22 20:02:45 -0300

    changing the activities

--- lab6/at1.asm
+++ lab6/at1.asm
@@ -1,76 +1,102 @@
 .data
-    	A: .space 40000   	# Vetor A com capacidade para 10000 elementos de precisão simples (4 bytes cada)
-    	B: .space 40000   	# Vetor B com capacidade para 10000 elementos de precisão simples (4 bytes cada)
-    	newline: .asciiz "\n"   # String contendo apenas uma nova linha
+    newline: .asciiz "\n"   # String contendo apenas uma nova linha
 
 .text
     Main:
-    	li $v0, 5          	# Solicita ao usuário um inteiro
-    	syscall
-    	move $s0, $v0      	# Armazena o valor digitado (n) em $s0
-    	move $a0, $s0      	# Passa n como argumento para o procedimento Media
+        li $v0, 5           # Solicita ao usuário um inteiro
+        syscall
+        move $s0, $v0       # Armazena o valor digitado (n) em $s0
+        
+        move $a0, $s0       # Passa n como argumento para o procedimento Media
+        li $t0, 4           # Cada elemento é um float (4 bytes)
+        mul $a0, $a0, $t0   # Calcula o tamanho necessário em bytes
 
-    	li $s3, 0          	# Inicializa o índice i
-    	sll $s0, $s0, 2    	# Calcula o deslocamento para o tamanho do vetor em bytes
+        # Aloca espaço para o vetor A
+        li $v0, 9           # Syscall para alocação de memória
+        move $a0, $a0       # Número de bytes a serem alocados
+        syscall
+        move $t1, $v0       # Endereço do vetor A
+
+        # Aloca espaço para o vetor B
+        li $v0, 9           # Syscall para alocação de memória
+        move $a0, $a0       # Número de bytes a serem alocados
+        syscall
+        move $t2, $v0       # Endereço do vetor B
+
+        li $s3, 0           # Inicializa o índice i
 
     Loop1:
-    	beq $s3, $s0, OutLoop1  # Verifica se o índice i ultrapassou o tamanho do vetor
+        beq $s3, $s0, OutLoop1  # Verifica se o índice i ultrapassou o tamanho do vetor
 
-    	li $v0, 6          	# Solicita ao usuário um float
-    	syscall	
-    	s.s $f0, A($s3)    	# Armazena o float lido em A[i]
+        li $v0, 6           # Solicita ao usuário um float
+        syscall
+        sll $t3, $s3, 2     # Calcula o deslocamento para o índice atual
+        add $t4, $t1, $t3   # Calcula o endereço de A[i]
+        s.s $f0, 0($t4)     # Armazena o float lido em A[i]
 
-    	li $v0, 6          	# Solicita ao usuário um float
-    	syscall
-    	s.s $f0, B($s3)    	# Armazena o float lido em B[i]
+        li $v0, 6           # Solicita ao usuário um float
+        syscall
+        add $t4, $t2, $t3   # Calcula o endereço de B[i]
+        s.s $f0, 0($t4)     # Armazena o float lido em B[i]
 
-    	addi $s3, $s3, 4   	# Incrementa o índice i
-    	j Loop1             	# Repete o loop
+        addi $s3, $s3, 1    # Incrementa o índice i
+        j Loop1             # Repete o loop
 
     OutLoop1:
-    	jal Media           	# Chama o procedimento Media
-    	j Exit             	 # Salta para a saída do programa
+        move $a0, $s0       # Passa n como argumento para o procedimento Media
+        move $a1, $t1       # Passa o endereço base de A
+        move $a2, $t2       # Passa o endereço base de B
+        jal Media           # Chama o procedimento Media
+        j Exit              # Salta para a saída do programa
 
     Media:
-    	li $s0, 0          	# Inicializa o índice i
-    	move $s1, $a0      	# Move n para $s1
-    	sll $s1, $s1, 2    	# Calcula o deslocamento para o tamanho do vetor em bytes
+        li $s0, 0           # Inicializa o índice i
+        move $s1, $a0       # Move n para $s1
+        move $t1, $a1       # Endereço base de A
+        move $t2, $a2       # Endereço base de B
+        # Inicializa a soma de A e B com 0.0
+        li $t5, 0
+        mtc1 $t5, $f1
+        mtc1 $t5, $f2
 
     Loop2:
-    	beq $s0, $s1, OutLoop2  # Verifica se o índice i ultrapassou o tamanho do vetor
+        beq $s0, $s1, OutLoop2  # Verifica se o índice i ultrapassou o tamanho do vetor
 
-    	l.s $f0, A($s0)    	# Carrega A[i]
-    	add.s $f1, $f1, $f0    	# Acumula A[i]
+        sll $t3, $s0, 2     # Calcula o deslocamento para o índice atual
+        add $t4, $t1, $t3   # Calcula o endereço de A[i]
+        l.s $f0, 0($t4)     # Carrega A[i]
+        add.s $f1, $f1, $f0 # Acumula A[i]
 
-    	l.s $f0, B($s0)    	# Carrega B[i]
-    	add.s $f2, $f2, $f0   	# Acumula B[i]
+        add $t4, $t2, $t3   # Calcula o endereço de B[i]
+        l.s $f0, 0($t4)     # Carrega B[i]
+        add.s $f2, $f2, $f0 # Acumula B[i]
 
-    	addi $s0, $s0, 4   	# Incrementa o índice i
-    	j Loop2             	# Repete o loop
+        addi $s0, $s0, 1    # Incrementa o índice i
+        j Loop2             # Repete o loop
 
     OutLoop2:
-    	mtc1 $a0, $f3      	# Move n para um registrador de ponto flutuante
-    	cvt.s.w $f3, $f3   	# Converte n para ponto flutuante
+        mtc1 $a0, $f3       # Move n para um registrador de ponto flutuante
+        cvt.s.w $f3, $f3    # Converte n para ponto flutuante
 
-    	div.s $f5, $f1, $f3    	# Calcula a média de A
-    	div.s $f6, $f2, $f3    	# Calcula a média de B
+        div.s $f5, $f1, $f3 # Calcula a média de A
+        div.s $f6, $f2, $f3 # Calcula a média de B
 
-    	li $v0, 4           	# Código para imprimir uma string
-    	la $a0, newline     	# Carrega o endereço da nova linha
-    	syscall             	# Chama a syscall para imprimir
+        li $v0, 4           # Código para imprimir uma string
+        la $a0, newline     # Carrega o endereço da nova linha
+        syscall             # Chama a syscall para imprimir
 
-    	li $v0, 2           	# Código para imprimir um float
-    	mov.s $f12, $f5     	# Move a média de A para $f12
-    	syscall             	# Chama a syscall para imprimir
-	
-    	li $v0, 4           	# Código para imprimir uma string
-    	la $a0, newline     	# Carrega o endereço da nova linha
-    	syscall             	# Chama a syscall para imprimir
+        li $v0, 2           # Código para imprimir um float
+        mov.s $f12, $f5     # Move a média de A para $f12
+        syscall             # Chama a syscall para imprimir
 
-    	li $v0, 2           	# Código para imprimir um float
-    	mov.s $f12, $f6     	# Move a média de B para $f12
-    	syscall             	# Chama a syscall para imprimir
+        li $v0, 4           # Código para imprimir uma string
+        la $a0, newline     # Carrega o endereço da nova linha
+        syscall             # Chama a syscall para imprimir
 
-    	jr $ra              	# Retorna ao chamador
+        li $v0, 2           # Código para imprimir um float
+        mov.s $f12, $f6     # Move a média de B para $f12
+        syscall             # Chama a syscall para imprimir
+
+        jr $ra              # Retorna ao chamador
 
 Exit:
--- lab6/at2.asm
+++ lab6/at2.asm
@@ -1,47 +1,63 @@
 .data
-    	newline: .asciiz "\n"   # String contendo apenas uma nova linha
+    newline: .asciiz "\n"   # String contendo apenas uma nova linha
 
 .text
-    	li $v0, 5          	# Solicita ao usuário um inteiro
-    	syscall
-    	move $s0, $v0      	# Armazena o valor digitado (n) em $s0
+    .globl main
 
-    Loop:
-        beq $s1, $s0, OutLoop   # Verifica se s1 (índice do loop) é igual a n
+main:
+    li $v0, 5           # Solicita ao usuário um inteiro
+    syscall
+    move $s0, $v0       # Armazena o valor digitado (n) em $s0
 
-        li $v0, 6          	# Solicita ao usuário um float
-        syscall
-        add.s $f1, $f1, $f0    	# Acumula o valor do float em $f1
+    li $t5, 0           # Inicializa a soma de A com 0
+    mtc1 $t5, $f1
+    mtc1 $t5, $f2       # Inicializa a soma de B com 0
+    li $s3, 0           # Inicializa o índice i
 
-        li $v0, 6          	# Solicita ao usuário um float
-        syscall
-        add.s $f2, $f2, $f0    	# Acumula o valor do float em $f2
+Loop:
+    beq $s3, $s0, OutLoop  # Verifica se o índice i ultrapassou o tamanho do vetor
 
-        addi $s1, $s1, 1   	# Incrementa o índice do loop
-        j Loop             	# Repete o loop
+    li $v0, 6           # Solicita ao usuário um float
+    syscall
+    add.s $f1, $f1, $f0 # Acumula o valor lido em A
 
-    OutLoop:
-        mtc1 $s0, $f3      	# Move o valor de n para $f3
-        cvt.s.w $f3, $f3   	# Converte o valor de n para float em $f3
+    li $v0, 6           # Solicita ao usuário um float
+    syscall
+    add.s $f2, $f2, $f0 # Acumula o valor lido em B
 
-        div.s $f5, $f1, $f3    	# Calcula a média de $f1 dividido por n
-        div.s $f6, $f2, $f3    	# Calcula a média de $f2 dividido por n
+    addi $s3, $s3, 1    # Incrementa o índice i
+    j Loop              # Repete o loop
 
-        # Exibe uma nova linha
-        li $v0, 4           	# Código para imprimir uma string
-        la $a0, newline     	# Carrega o endereço da nova linha
-        syscall             	# Chama a syscall para imprimir
+OutLoop:
+    move $a0, $s0       # Passa n como argumento para o procedimento Media
+    jal Media           # Chama o procedimento Media
+    j Exit              # Salta para a saída do programa
 
-        li $v0, 2           	# Código para imprimir um float
-        mov.s $f12, $f5     	# Move a média de $f1 para $f12
-        syscall             	# Chama a syscall para imprimir
+Media:
+    mtc1 $a0, $f3       # Move n para um registrador de ponto flutuante
+    cvt.s.w $f3, $f3    # Converte n para ponto flutuante
 
-        # Exibe uma nova linha
-        li $v0, 4           	# Código para imprimir uma string
-        la $a0, newline     	# Carrega o endereço da nova linha
-        syscall             	# Chama a syscall para imprimir
+    div.s $f5, $f1, $f3 # Calcula a média de A
+    div.s $f6, $f2, $f3 # Calcula a média de B
 
-        li $v0, 2           	# Código para imprimir um float
-        mov.s $f12, $f6     	# Move a média de $f2 para $f12
-        syscall             	# Chama a syscall para imprimir
+    li $v0, 4           # Código para imprimir uma string
+    la $a0, newline     # Carrega o endereço da nova linha
+    syscall             # Chama a syscall para imprimir
 
+    li $v0, 2           # Código para imprimir um float
+    mov.s $f12, $f5     # Move a média de A para $f12
+    syscall             # Chama a syscall para imprimir
+
+    li $v0, 4           # Código para imprimir uma string
+    la $a0, newline     # Carrega o endereço da nova linha
+    syscall             # Chama a syscall para imprimir
+
+    li $v0, 2           # Código para imprimir um float
+    mov.s $f12, $f6     # Move a média de B para $f12
+    syscall             # Chama a syscall para imprimir
+
+    jr $ra              # Retorna ao chamador
+
+Exit:
+    li $v0, 10          # Código para encerrar o programa
+    syscall             # Chama a syscall para encerrar
