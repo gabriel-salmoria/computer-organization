74bc5924086182c2fdd9e906fa9407a9b69407b2
gabriel-salmoria <gabrielsalmoria1@gmail.com>
2024-06-21 19:09:21 -0300

    clean code, uncle bob is now proud

--- lab8/at1.asm
+++ lab8/at1.asm
@@ -1,10 +1,50 @@
 .data
     MAX: .word 3
     
-    A: .float 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0     
-    B: .float 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0
+    A: .space 1024
+    B: .space 1024
 
 .text
+				# Preencher a matriz A
+    		la $a0, A     # Passa o endereço base da matriz A no registrador $a0
+    		jal preencher     # Chama a função para preencher a matriz
+
+		    # Preencher a matriz B
+  		  la $a0, B     # Passa o endereço base da matriz B no registrador $a0
+	  	  jal preencher     # Chama a função para preencher a matriz
+
+				j main
+
+		preencher:
+    		move $t1, $a0       # Move o endereço base da matriz para $t1
+    		li $t0, 0           # Inicializa o contador de elementos em 0
+    		li $t2, 0           # Inicializa o índice da coluna em 0
+
+		preencher_colunas:
+    		li $t3, 0           # Inicializa o índice da linha em 0
+
+		preencher_linhas:
+  		  mtc1 $t0, $f0							      # Move o valor do contador para o registrador de ponto flutuante $f0
+		    cvt.s.w $f0, $f0 							  # Converte o inteiro em ponto flutuante
+
+		    mul $t4, $t3, 16 							  # Calcula o deslocamento da linha (linha * 16)
+		    add $t4, $t4, $t2							  # Adiciona o deslocamento da coluna (linha * 16 + coluna)
+		    sll $t4, $t4, 2							    # Multiplica o deslocamento por 4 (cada float tem 4 bytes)
+		    add $t5, $t1, $t4							  # Calcula o endereço do elemento na matriz
+
+		    swc1 $f0, 0($t5)						    # Armazena o valor do ponto flutuante no endereço calculado
+
+    		addi $t0, $t0, 1  						  # Incrementa o contador
+	   		addi $t3, $t3, 1	    					# Incrementa o índice da linha
+		    bne $t3, 16, preencher_linhas  	# Se não percorreu todas as linhas, continue
+
+    		addi $t2, $t2, 1						    # Incrementa o índice da coluna
+    		bne $t2, 16, preencher_colunas  # Se não percorreu todas as colunas, continue
+
+    		jr $ra              # Retorna para a chamada anterior
+
+
+
     main:
         # Inicializa MAX
         la   $t0, MAX
